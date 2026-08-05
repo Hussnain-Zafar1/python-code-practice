3dfa4cd19dc298564e57f87a0fa15c03dce1310b
Hussnain-Zafar1 <zhussnain682@gmail.com>
2026-08-05 20:12:24 +0500

    stacks and queues using lists

--- internship/week1/day1/list.py
+++ internship/week1/day1/list.py
@@ -1,4 +1,8 @@
+"""
+practicing lists as a stack and queue
+"""
 
+from collections import deque
 
 #append() method
 fruits = ["apple", "banana", "cherry"]
@@ -33,4 +37,16 @@ fruits = ["apple", "banana", "berry",["mango","kiwi"]]
 fruits_copy = fruits.copy()
 # print(fruits_copy)
 fruits_copy[3].append("orange")
+print(fruits)
+
+
+# queue implementation
+fruits = deque(["apple", "banana", "berry"])
+fruits.append("kiwi")
+print(fruits)
+fruits.append("orange")
+print(fruits)
+fruits.popleft()
+print(fruits)
+fruits.popleft()
 print(fruits)
